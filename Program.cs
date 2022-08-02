1ede4b3dfa95276bc01b6ae9e42f4f40bb14254d
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2022-08-01 22:37:26 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tarea_1_progra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+
+            string name = "Jhonatan";
+            string edad = "19";
+            // Impresión de datos h
+            Console.WriteLine("La suma es:" + total);
+            Console.WriteLine(name + " " + edad);
+        }
+    }
+}
